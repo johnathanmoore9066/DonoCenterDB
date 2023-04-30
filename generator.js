b8bb3a82ef0abe3ecb6e4bca63e50618e22140e5
johnathanmoore9066 <johnathanmoore9066@gmail.com>
2023-04-30 18:33:29 -0400

    refactored code top populate tables

--- generator.js
+++ generator.js
@@ -1,139 +1,88 @@
 document.addEventListener("DOMContentLoaded", () => {
-
-
-    var xhr = new XMLHttpRequest();
-    var url = "donors.json";
-    
-    xhr.open("GET", url, true);
-    
-    xhr.responseType = "json";
-    
-    xhr.onload = ()=> {
-    if (xhr.status == 200) {
-    var data = xhr.response;
-    console.log(xhr.status);
-
-const tableBody = document.querySelector("#report");
-const theadRow = tableBody.querySelector("thead");
-const headers = Array.from(theadRow.querySelectorAll("th")).slice(1);
-const donors = data.donors;
-
-donors.forEach((donor) => {
-    const row = document.createElement("tr");
-    const donorDate = document.createElement("td");
-    const dateObj = new Date(donor.Date);
-    donorDate.textContent = dateObj.toLocaleDateString("en-US", {year: 'numeric', month: 'short', day: 'numeric' });
-    row.appendChild(donorDate)
-    const donorContact = document.createElement("td");
-    donorContact.textContent = donor.Contact;
-    row.appendChild(donorContact)
-    const donorPhone = document.createElement("td");
-    donorPhone.textContent = donor.Phone;
-    row.appendChild(donorPhone)
-    const donorEmail = document.createElement("td");
-    donorEmail.textContent = donor.Email;
-    row.appendChild(donorEmail)
-    const donorAddress = document.createElement("td");
-    donorAddress.textContent = donor.Address;
-    row.appendChild(donorAddress)
-    const donorAmount = document.createElement("td");
-    const amount = parseFloat(donor.Amount); // Convert the string to a number
-    const amountObj = amount.toLocaleString("en-US", {
-      style: "currency",
-      currency: "USD",
-    });
-    donorAmount.textContent = amountObj;
-    row.appendChild(donorAmount)
-
-    
-
-    tableBody.appendChild(row);
-});
-
-const change = document.getElementById("change");
-change.addEventListener("change", sortTable);
-
-function sortTable() {
-    const selectedOption = change.value;
-
-    donors.sort((a, b) => {
-        if (selectedOption === "Date") {
-            return new Date(a.Date) - new Date(b.Date);
-        } else if (selectedOption === "Contact"){
-            return a.Contact.localeCompare(b.Contact);
-        } else if (selectedOption === "Amount"){
-            return a.Amount - b.Amount;
-        }
-    });
-        headers.forEach((header, index) => {
-            const headerCell = document.createElement("th");
-        headerCell.textContent = header.textContent;
-        theadRow.appendChild(headerCell);
-
-      });
-
-    clearTable();
-    populateTable(donors);
-};
-
-function clearTable() {
-    while (tableBody.firstChild) {
-        tableBody.removeChild(tableBody.firstChild);
+    const url = "donors.json";
+  
+    function fetchDonors() {
+      return fetch(url)
+        .then(response => response.json())
+        .then(data => {
+          const donors = data.donors;
+          populateTable(donors);
+          setupSort(donors);
+        })
+        .catch(error => console.log(error));
     }
-}
-
-function populateTable(donors) {
-
-    const thead = document.createElement("thead");
-    const headerRow = document.createElement("tr");
-    const headers = ["Date", "Contact", "Phone", "Email", "Address", "Amount"];
-
-    headers.forEach((header) => {
+  
+    function setupSort(donors) {
+      const change = document.getElementById("change");
+      change.addEventListener("change", () => sortTable(donors, change.value));
+    }
+  
+    function sortTable(donors, selectedOption) {
+      donors.sort((a, b) => {
+        if (selectedOption === "Date") {
+          return new Date(a.Date) - new Date(b.Date);
+        } else if (selectedOption === "Contact"){
+          return a.Contact.localeCompare(b.Contact);
+        } else if (selectedOption === "Amount"){
+          return a.Amount - b.Amount;
+        }
+      });
+      clearTable();
+      populateTable(donors);
+    }
+  
+    function clearTable() {
+      const tableBody = document.querySelector("#report");
+      while (tableBody.firstChild) {
+        tableBody.removeChild(tableBody.firstChild);
+      }
+    }
+  
+    function populateTable(donors) {
+      const tableBody = document.querySelector("#report");
+      const theadRow = document.createElement("tr");
+      const headers = ["Date", "Contact", "Phone", "Email", "Address", "Amount"];
+  
+      headers.forEach((header) => {
         const th = document.createElement("th");
         th.textContent = header;
-        headerRow.appendChild(th);
-    });
-
-    thead.appendChild(headerRow);
-    tableBody.appendChild(thead);
-
-donors.forEach((donor) => {
-    const row = document.createElement("tr");
-    const donorDate = document.createElement("td");
-    donorDate.textContent = donor.Date;
-    const dateObj = new Date(donor.Date);
-    donorDate.textContent = dateObj.toLocaleDateString("en-US", {year: 'numeric', month: 'short', day: 'numeric' });
-    row.appendChild(donorDate)
-    const donorContact = document.createElement("td");
-    donorContact.textContent = donor.Contact;
-    row.appendChild(donorContact)
-    const donorPhone = document.createElement("td");
-    donorPhone.textContent = donor.Phone;
-    row.appendChild(donorPhone)
-    const donorEmail = document.createElement("td");
-    donorEmail.textContent = donor.Email;
-    row.appendChild(donorEmail)
-    const donorAddress = document.createElement("td");
-    donorAddress.textContent = donor.Address;
-    row.appendChild(donorAddress)
-    const donorAmount = document.createElement("td");
-    const amount = parseFloat(donor.Amount); // Convert the string to a number
-    const amountObj = amount.toLocaleString("en-US", {
-      style: "currency",
-      currency: "USD",
-    });
-    donorAmount.textContent = amountObj;
-    row.appendChild(donorAmount)
-
-    tableBody.appendChild(row);
-});
-
-}
-}
-
-};
-
-
-xhr.send();
-});
-
+        theadRow.appendChild(th);
+      });
+  
+      const thead = document.createElement("thead");
+      thead.appendChild(theadRow);
+      tableBody.appendChild(thead);
+  
+      donors.forEach((donor) => {
+        const row = document.createElement("tr");
+        const donorDate = document.createElement("td");
+        const dateObj = new Date(donor.Date);
+        donorDate.textContent = dateObj.toLocaleDateString("en-US", {year: 'numeric', month: 'short', day: 'numeric' });
+        row.appendChild(donorDate)
+        const donorContact = document.createElement("td");
+        donorContact.textContent = donor.Contact;
+        row.appendChild(donorContact)
+        const donorPhone = document.createElement("td");
+        donorPhone.textContent = donor.Phone;
+        row.appendChild(donorPhone)
+        const donorEmail = document.createElement("td");
+        donorEmail.textContent = donor.Email;
+        row.appendChild(donorEmail)
+        const donorAddress = document.createElement("td");
+        donorAddress.textContent = donor.Address;
+        row.appendChild(donorAddress)
+        const donorAmount = document.createElement("td");
+        const amount = parseFloat(donor.Amount); // Convert the string to a number
+        const amountObj = amount.toLocaleString("en-US", {
+          style: "currency",
+          currency: "USD",
+        });
+        donorAmount.textContent = amountObj;
+        row.appendChild(donorAmount)
+  
+        tableBody.appendChild(row);
+      });
+    }
+  
+    fetchDonors();
+  });
--- populate_table.js
+++ populate_table.js
@@ -1,103 +1,63 @@
-// Create an XMLHttpRequest object
-var xhr = new XMLHttpRequest();
+// Define constants
+const INVENTORY_URL = "inventory.json";
+const DATE_FORMAT_OPTIONS = { year: 'numeric', month: 'numeric', day: 'numeric', hour: 'numeric', minute: 'numeric', hour12: true };
 
-// Open the connection to the JSON file
-xhr.open("GET", "inventory.json", true);
+// Fetch inventory data from server
+fetch(INVENTORY_URL)
+  .then(response => response.json())
+  .then(data => {
+    // Get table elements
+    const table = document.querySelector("table.inventory");
+    const thead = table.querySelector("thead");
+    const tbody = table.querySelector("tbody");
 
-// Set the response format to JSON
-xhr.responseType = "json";
+    // Create table header row
+    const headerRow = document.createElement("tr");
+    for (let key in data.items[0]) {
+      const headerCell = document.createElement("th");
+      headerCell.textContent = key.replace(/_/g, ' ');
+      headerCell.addEventListener('click', () => sortTable(key));
+      headerRow.appendChild(headerCell);
+    }
+    thead.appendChild(headerRow);
 
-// Specify what to do when the response is loaded
-xhr.onload = function() {
-  // Get the JSON data
-  let data = xhr.response;
-
-  // Get the table header element
-  let thead = document.querySelector("table.inventory thead");
-
-  // Create a new row for the header
-  let headerRow = document.createElement("tr");
-
-  // Loop through the keys of the first item in the data array
-  for (let key in data.items[0]) {
-    // Create a new header cell with the key text as the content
-    const headerCell = document.createElement("th");
-    headerCell.textContent = key.replace(/_/g, ' '); // Replace underscores with spaces
-
-    // Add an event listener to sort the table by clicking on the column header
-    headerCell.addEventListener('click', function() {
-      sortTable(this.cellIndex);
+    // Create table rows
+    data.items.forEach(item => {
+      const row = document.createElement("tr");
+      for (let key in item) {
+        const cell = document.createElement("td");
+        cell.textContent = item[key];
+        if (key === 'Last_updated') {
+          const currentDate = new Date();
+          cell.textContent = currentDate.toLocaleDateString("en-US", DATE_FORMAT_OPTIONS);
+        }
+        row.appendChild(cell);
+      }
+      tbody.appendChild(row);
     });
 
-    headerRow.appendChild(headerCell);
-  }
+    // Sort table by default column
+    sortTable(Object.keys(data.items[0])[0]);
+  })
+  .catch(error => console.error(error));
 
-  // Append the header row to the table header
-  thead.appendChild(headerRow);
-
-  // Get the table body element
-  const tbody = document.querySelector("table.inventory tbody");
-
-  // Loop through the data and create table rows
-  for (let i = 0; i < data.items.length; i++) {
-    // Create a new row
-    const row = document.createElement("tr");
-
-    // Loop through the keys of the current item
-    for (let key in data.items[i]) {
-      // Create a new cell with the current item value as the content
-      var cell = document.createElement("td");
-      cell.textContent = data.items[i][key];
-
-      // Add current date and time to the "Last updated" column
-      if (key === 'Last_updated') {
-        const currentDate = new Date();
-        const timeOptions = { hour: 'numeric', minute: 'numeric', hour12: true };
-        cell.textContent = currentDate.toLocaleDateString("en-US", {year: 'numeric', month: 'numeric', day: 'numeric' }) + " " + currentDate.toLocaleTimeString("en-US", timeOptions);
-      }
-
-      row.appendChild(cell);
-    }
-
-    // Append the row to the table body
-    tbody.appendChild(row);
-  }
-
-  // Sort the table by the first column on initial load
-  sortTable(0);
-};
-
-// Send the request
-xhr.send();
-
-// Function to sort the table by a given column index
-function sortTable(columnIndex) {
+// Function to sort the table by a given column key
+function sortTable(columnKey) {
   const table = document.querySelector("table.inventory");
   const tbody = table.querySelector("tbody");
   const rows = Array.from(tbody.querySelectorAll("tr"));
 
-  // Sort the rows based on the data in the selected column
-  rows.sort(function(a, b) {
-    const aCellValue = a.querySelector("td:nth-child(" + (columnIndex + 1) + ")").textContent;
-    const bCellValue = b.querySelector("td:nth-child(" + (columnIndex + 1) + ")").textContent;
+  rows.sort((a, b) => {
+    const aCellValue = a.querySelector(`td[data-key="${columnKey}"]`).textContent;
+    const bCellValue = b.querySelector(`td[data-key="${columnKey}"]`).textContent;
 
-    // If the cell value is a number, convert it to a number for proper sorting
     if (!isNaN(aCellValue) && !isNaN(bCellValue)) {
-      aCellValue = parseFloat(aCellValue);
-      bCellValue = parseFloat(bCellValue);
-    }
-
-    if (aCellValue > bCellValue) {
-      return 1;
-    } else if (aCellValue < bCellValue) {
-      return -1;
+      return parseFloat(aCellValue) - parseFloat(bCellValue);
     } else {
-      return 0;
+      return aCellValue.localeCompare(bCellValue);
     }
   });
-
-  // Append the sorted rows back to the table body
-  for (var i = 0; i < rows.length; i++) {
-    tbody.appendChild(rows[i]);
-  }
+  
+  tbody.innerHTML = "";
+  rows.forEach(row => tbody.appendChild(row));
 }
