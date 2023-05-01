25fbf0eedd913cb90b3619ddeb6fdb9f0a73a7db
johnathanmoore9066 <johnathanmoore9066@gmail.com>
2023-05-01 10:07:14 -0400

    update inventory style

--- populate_table.js
+++ populate_table.js
@@ -1,11 +1,40 @@
 // Define constants
 const INVENTORY_URL = "inventory.json";
 const DATE_FORMAT_OPTIONS = { year: 'numeric', month: 'numeric', day: 'numeric', hour: 'numeric', minute: 'numeric', hour12: true };
+const addItem = document.getElementById("addItem");
+const modifyItem = document.getElementById("modifyItem");
+const deleteItem = document.getElementById("deleteItem");
+
 
 // Fetch inventory data from server
 fetch(INVENTORY_URL)
   .then(response => response.json())
   .then(data => {
+    // Add item to inventory
+    addItem.addEventListener('click', () => {
+      const addItemForm = document.getElementById("addItemForm");
+      addItemForm.innerHTML = `
+        <input type= "text" id=itemInput" placeholder="Item" required>
+        <input type= "number" id=quantityInput" placeholder="Quantity" required>
+        <input type= "text" id="locationInput" placeholder="Location" required> 
+        <button type="submit" id="submit">Add</button>
+      `;
+      addItemForm.addEventListener('submit', (event) => {
+        event.preventDefault();
+        const newItem = {
+          "Item": document.getElementById("itemInput").value,
+          "Qty": document.getElementById("quantityInput").value,
+          "Location": document.getElementById("locationInput").value,
+          "Last_updated": new Date().toLocaleDateString("en-US", DATE_FORMAT_OPTIONS),
+          "Updated_by": "User"
+        };
+        data.items.push(newItem);
+        updateInventory(data);
+        
+      
+    })
+  });
+
     // Get table elements
     const table = document.querySelector("table.inventory");
     const thead = table.querySelector("thead");
